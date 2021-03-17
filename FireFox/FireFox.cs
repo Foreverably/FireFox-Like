da85b2f3745753ff7a19d9ebc39a7c50a81f31de
Yeet Yeet <cjhere09@gmail.com>
2021-03-17 08:23:28 -0500

    Added EasyTabs and Cefsharp Chrome tabs added

--- FireFox/FireFox.cs
+++ FireFox/FireFox.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using EasyTabs;
 
 namespace FireFox
 {
-    public partial class FireFox : Form
+    public partial class FireFox : TitleBarTabs
     {
         public FireFox()
         {
             InitializeComponent();
+            AeroPeekEnabled = false;
+            TabRenderer = new ChromeTabRenderer(this);
+        }
+
+        private void FireFox_Load(object sender, EventArgs e)
+        {
+
+        }
+        public override TitleBarTab CreateTab()
+        {
+            return new TitleBarTab(this)
+            {
+                Content = new Form1 { Text = "New Tab" }
+            };
         }
     }
 }
--- FireFox/Program.cs
+++ FireFox/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using EasyTabs;
 
 namespace FireFox
 {
@@ -16,7 +17,20 @@ namespace FireFox
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            FireFox container = new FireFox();
+            container.Tabs.Add(new EasyTabs.TitleBarTab(container)
+            {
+                Content = new Form1
+                {
+                    Text = "New Tab"
+                }
+            });
+
+            container.SelectedTabIndex = 0;
+            TitleBarTabsApplicationContext applicationContext = new TitleBarTabsApplicationContext();
+            applicationContext.Start(container);
+            Application.Run(applicationContext);
         }
     }
 }
